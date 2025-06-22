7c7eeb9cb4d8f309a8e4ac371fc8c83edc6436e5
Richard <vinicys13@gmail.com>
2025-06-22 11:10:42 -0300

    pagina index

---
+++ server.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+const path = require('path');
+const routes = require('./routes');
+
+app.use(routes);
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
+
+app.listen(3000, () =>{
+    console.log('Servidor rodando na porta 3000');
+        console.log('Acessar http://localhost:3000');
+})
